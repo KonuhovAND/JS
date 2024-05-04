227fc6650f1af7600b6864033586deb3a14a675f
KonuhovAND <107177236+KonuhovAND@users.noreply.github.com>
2024-05-04 19:08:48 +0300

    Add files via upload

---
+++ privet_mir.js
@@ -0,0 +1,19 @@
+'use strict';
+function Accumulator(value){
+  this.value = value;
+  this.read = function(){
+    this.value += +prompt();
+  this.bab = '123';
+  }
+}
+let listt = {
+  asd:'pisa',
+  ssd:'popa',
+};
+function sayHay(x){
+  return x++;
+};
+let x =0;
+let cac = new Accumulator(1);
+alert(listt.asd?.sayHay(x));
+alert(`${x}`);
